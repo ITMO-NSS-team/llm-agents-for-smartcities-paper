4e40284e54bc9a4ac418465c021f3a2c4090b8c5
themarevampire <andrey.getmanov280299@yandex.ru>
2024-09-30 19:56:33 +0300

    Everything in russian (except datasets) translated into English

--- modules/variables/prompts/prompts.py
+++ modules/variables/prompts/prompts.py
@@ -11,12 +11,8 @@ it should be added in the answer.
 mention for what field there was no data.
 5. If you do not know how to answer the questions, say so.
 6. Before give an answer to the user question, provide explanation. Mark the answer
-with keyword 'ANSWER', and explanation with 'EXPLANATION'. Both answer and explanation must
-be in Russian language.
-7. If the question is about complaints, answer about at least 5 complaints topics.
-8. Answer should be three sentences maximum.
-For each sentence in English language you will be fined for 100$, so in answers you must
-use only Russian language.
+with keyword 'ANSWER', and explanation with 'EXPLANATION'.
+7. Answer should be three sentences maximum.
 """
 
 strategy_sys_prompt = r"""Answer the question following rules below. For answer you must
